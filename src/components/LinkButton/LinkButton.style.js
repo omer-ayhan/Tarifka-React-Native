3d0a7fe0ac58e03b6ca0e879edb257cc03cc9741
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-03 17:31:12 +0300

    style(styling): :lipstick: styling for LİnkButton component

--- src/components/LinkButton/LinkButton.style.js
+++ src/components/LinkButton/LinkButton.style.js
@@ -1,3 +1,17 @@
 import { StyleSheet } from "react-native";
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  button: {
+    margin: 20,
+    backgroundColor: "#b71c1c",
+    borderRadius: 10,
+    padding: 10,
+  },
+  text: {
+    fontSize: 22,
+    fontWeight: "bold",
+    color: "#fff",
+    textAlign: "center",
+  },
+});
+export default styles;
