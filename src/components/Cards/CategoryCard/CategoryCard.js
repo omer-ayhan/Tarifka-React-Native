58216cfc04f798df3ae7cf027e4f2086b8ebbb10
Ömer Ayhan <om.ayhan247@gmail.com>
2021-12-03 13:36:58 +0300

    feat(funcitonality): :sparkles: Meals page
    fetching functionality and flatlist component

--- src/components/Cards/CategoryCard/CategoryCard.js
+++ src/components/Cards/CategoryCard/CategoryCard.js
@@ -1,13 +1,21 @@
 import React from "react";
-import { View, Text, Image } from "react-native";
+import { useNavigation } from "@react-navigation/core";
+import { View, Text, Image, TouchableWithoutFeedback } from "react-native";
 import styles from "./CategoryCard.style";
 
 export default function CategoryCard({ categoryData }) {
+  const navigation = useNavigation();
+
   const { strCategoryThumb, strCategory } = categoryData;
+  const handleCategory = () =>
+    navigation.navigate("Meals", { category: strCategory });
+
   return (
-    <View style={styles.container}>
-      <Image style={styles.image} source={{ uri: strCategoryThumb }} />
-      <Text style={styles.title}>{strCategory}</Text>
-    </View>
+    <TouchableWithoutFeedback onPress={handleCategory}>
+      <View style={styles.container}>
+        <Image style={styles.image} source={{ uri: strCategoryThumb }} />
+        <Text style={styles.title}>{strCategory}</Text>
+      </View>
+    </TouchableWithoutFeedback>
   );
 }
--- src/pages/Meals/Meals.js
+++ src/pages/Meals/Meals.js
@@ -1,10 +1,25 @@
+import { useRoute } from "@react-navigation/core";
 import React from "react";
-import { View, Text } from "react-native";
+import { View, Text, FlatList } from "react-native";
+import useFetch from "../../hooks/useFetch";
+import Config from "react-native-config";
+import MealCard from "../../components/Cards/MealCard";
 
 export default function Meals() {
-  return (
-    <View>
-      <Text>Meals</Text>
-    </View>
-  );
+  const router = useRoute();
+
+  const url = `${Config.API_URL}/filter.php?c=${router.params.category}`;
+  const { data, loading, error } = useFetch(url);
+
+  if (loading) {
+    return <Text>Loading...</Text>;
+  }
+
+  if (error) {
+    return <Text>{error}</Text>;
+  }
+
+  const renderMeals = ({ item }) => <MealCard mealData={item} />;
+
+  return <FlatList data={data.meals} renderItem={renderMeals} />;
 }
